73f12d5bac4b46aba3c5deed49f9e0efcc61f921
satish kumar <satish@blr-ad2.systech.sysfore.com>
2017-08-09 15:43:25 +0530

    chnages made

--- src/wp-config.php
+++ src/wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'wordpress');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'test123');
+define('DB_PASSWORD', 'test12');
 
 /** MySQL hostname */
 define('DB_HOST', 'mysql');
